e3c084bb80ba74d8b8e00420753f7bd8ab2eed89
nitrotap <kartikinpublic@gmail.com>
2022-03-03 23:18:16 -0700

    adding public files and setting up animal handlers

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 const express = require('express');
 const { animals } = require('./data/animals.json');
-const PORT = process.env.PORT || 443;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
@@ -12,6 +12,9 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
+// provide file path for static resources
+app.use(express.static('public'));
+
 
 
 function filterByQuery(query, animalsArray) {
@@ -119,9 +122,25 @@ app.post('/api/animals', (req, res) => {
 
 });
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
 
 app.listen(PORT, () => {
-    console.log(`API server now on port 3001!`);
+    console.log(`API server now on port ${PORT}!`);
 });
 
 
